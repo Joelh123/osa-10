6f70c920b04b6e0f32ced0b5028155f072b1f30d
Joel H. <143587522+Joelh123@users.noreply.github.com>
2025-12-04 13:10:47 +0200

    10.5

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,15 +1,78 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const RepositoryItem = ({ data }) => {
+	const styles = StyleSheet.create({
+		headerContainer: {
+			marginLeft: 8,
+			marginTop: 8,
+			backgroundColor: "white",
+			display: "flex",
+			flexDirection: "row",
+		},
+		image: {
+			height: 50,
+			width: 50,
+			borderRadius: 10,
+			overflow: "hidden",
+		},
+		language: {
+			backgroundColor: "blue",
+			color: "white",
+			padding: 5,
+			borderRadius: 4,
+			alignSelf: "flex-start",
+		},
+		statsContainer: {
+			marginLeft: 40,
+			marginTop: 8,
+			marginBottom: 10,
+			backgroundColor: "white",
+			display: "flex",
+			flexDirection: "row",
+			gap: 40,
+		},
+	});
+
 	return (
 		<View>
-			<Text>Fullname: {data.fullName}</Text>
-			<Text>Description: {data.description}</Text>
-			<Text>Language: {data.language}</Text>
-			<Text>Forks: {data.forksCount}</Text>
-			<Text>Stars: {data.stargazersCount}</Text>
-			<Text>Rating average: {data.ratingAverage}</Text>
-			<Text>Reviews: {data.reviewCount}</Text>
+			<View style={styles.headerContainer}>
+				<Image style={styles.image} source={{ uri: data.ownerAvatarUrl }} />
+				<View style={{ marginLeft: 10 }}>
+					<Text style={{ fontWeight: 700 }}>{data.fullName}</Text>
+					<Text>{data.description}</Text>
+					<Text style={styles.language}>{data.language}</Text>
+				</View>
+			</View>
+			<View style={styles.statsContainer}>
+				<View>
+					<Text style={{ fontWeight: 700 }}>
+						{data.forksCount >= 1000
+							? `${(data.forksCount / 1000).toFixed(1)}k`
+							: data.forksCount}
+					</Text>
+					<Text>Forks</Text>
+				</View>
+				<View>
+					<Text style={{ fontWeight: 700 }}>
+						{data.stargazersCount >= 1000
+							? `${(data.stargazersCount / 1000).toFixed(1)}k`
+							: data.stargazersCount}
+					</Text>
+					<Text>Stars</Text>
+				</View>
+				<View>
+					<Text style={{ fontWeight: 700 }}>{data.ratingAverage}</Text>
+					<Text>Rating</Text>
+				</View>
+				<View>
+					<Text style={{ fontWeight: 700 }}>
+						{data.reviewCount >= 1000
+							? `${(data.reviewCount / 1000).toFixed(1)}k`
+							: data.reviewCount}
+					</Text>
+					<Text>Reviews</Text>
+				</View>
+			</View>
 		</View>
 	);
 };
